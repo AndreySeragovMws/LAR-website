1eaf07602cd0b10693d77281a837361b9e746a57
Andrey Seragov <106803324+AndreySeragovMws@users.noreply.github.com>
2022-06-10 14:31:21 +0300

    Исправление замечаний
    Исправления по замечаниям от 09.06.2022.
    Изменены единицы измерения на % и rem.

--- js/swiper.js
+++ js/swiper.js
@@ -1,34 +1,25 @@
 const swiper = new Swiper('.blog__slider', {
-  
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  slidesPerView: "auto",
+  spaceBetween: 32,
   breakpoints: {
     // when window width is >= 320px
     320: {
       slidesPerView: 1.1,
-      spaceBetween: 20
     },
     450: {
       slidesPerView: 1.5,
-      spaceBetween: 20
     },
     680: {
       slidesPerView: 2,
-      spaceBetween: 20
     },
     786: {
       slidesPerView: 2.5,
-      spaceBetween: 30
     },
     1100: {
       slidesPerView: 3,
-      spaceBetween: 30
     },
     1440: {
       slidesPerView: 3,
-      spaceBetween: 32
     },
   }
 });
